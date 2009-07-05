0f0561b88b262956a122a9b8574b5bd86249ce20
pasdVn <pasdVn3@gmx.de>
2009-07-05 23:04:46 +0200

    Allgemeineres handling von Aura::HandleSchoolAbsorb() + Priester Talent "Zeit Schinden"
    Die Boni, die Absorbtionsauren durch Zaubermacht erhalten, können über ma
    ngos.spell_bonus_data (Datenbank)
    eingestellt werde. Außerdem kann dieser Bonus durch den Spellmodifier S
    PELLMOD_SPELL_BONUS_DAMAGE beeinflusst
    werde. Dadurch kann der zweite Effekt des Priestertalents "Zeit
    Schinden/Borrowed Time" (Dummy Aura) auf
    einfache Weise implementiert werden.
    
    --den sql Teil nicht vergessen--

---
+++ sql/updates/custom/patch_schoolAbsorb_pasdVn.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mangos`.`spell_bonus_data` (`entry` ,`direct_bonus` ,`dot_bonus` ,`ap_bonus` ,`comments`) VALUES 
+('6143', '0.1', '0', '0', 'Mage - Frost Ward'),
+('543', '0.1', '0', '0', 'Mage - Fire Ward');
