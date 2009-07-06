345dad218322e645b7c44e8affbfa396247adb76
pasdVn <pasdVn3@gmx.de>
2009-07-06 18:55:48 +0200

    added proc flags for "renewed hope" (57470 and ranks)

---
+++ sql/updates/custom/patch_renewed_hope_pasdVn.sql
@@ -0,0 +1,4 @@
+/*Renewed Hope*/
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(57470, 0, 6, 1, 0, 0, 0, 0, 0, 0, 0),
+(57472, 0, 6, 1, 0, 0, 0, 0, 0, 0, 0);
