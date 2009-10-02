aaa5c76fc162e247d39fd8722e2f141841613d16
pasdVn <pasdVn3@gmx.de>
2009-10-02 18:48:00 +0200

    reworked pet stats and dynamic stat scaling
    * added mindmg and maxdmg field to pet_levelstats
     * cleanup in Pet::InitStatsForLevel()
     * apply bonuses for pets without scaling aura only static - at summon
    	-> added ap bonus for "shadow fiend" (34433)
    	-> added bonuses and glyph modification (63271)
    	   for "Feral Spirit" wolves (51533)
    	-> added spelldamage bonus for "Mirror Image" (55342)
     * added support for basepoints calculation of dynamic pet scaling auras
    	-> corrected scaling values for hunter pets and implemented scaling
    	   modifications ("Wild Hunt" (62758 & ranks), "Hunter vs. Wild" (56339 & ranks))
    	-> added scaling factors for death knight ghoul and scaling modifications
    	   ("Glyph of the Ghoul" (58686)
    	-> added calculation for expertise and +hit scaling auras
     * removed redundant code of old scaling system in pet stat update methods

--- src/game/Player.h
+++ src/game/Player.h
@@ -1555,6 +1555,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         void CharmSpellInitialize();
         void PossessSpellInitialize();
         void RemovePetActionBar();
+        void UpdatePetScalingAuras();
 
         bool HasSpell(uint32 spell) const;
         bool HasActiveSpell(uint32 spell) const;            // show in spellbook
@@ -2569,6 +2570,8 @@ class MANGOS_DLL_SPEC Player : public Unit
         // Temporary removed pet cache
         uint32 m_temporaryUnsummonedPetNumber;
         uint32 m_oldpetspell;
+
+        uint32 m_petScalingUpdateTimer;
 
         AchievementMgr m_achievementMgr;
         ReputationMgr  m_reputationMgr;
